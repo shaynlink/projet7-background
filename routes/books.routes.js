77e6f48229f899f56767770f6623ac0ce2550216
shaynlink <guemmoudrayane03@gmail.com>
2023-08-10 03:30:21 +0200

    add routes

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/auth.controller');
+const booksCtrl = require('../controllers/books.controller');
+const multer = require('multer');
+const auth = require('../middlewares/auth');
+const checkInput = require('../middlewares/checkInput');
+const upload = multer();
 
-router.post('/login', userCtrl.login);
-//router.post('/signup', userCtrl.signup);
+router.get('/', booksCtrl.getBooks);
+router.get('/bestrating', booksCtrl.getBestRatingBooks);
+router.get('/:id', booksCtrl.getBookById);
+router.post('/', auth, upload.single('image'), checkInput.addBook, booksCtrl.addBook);
+router.post('/:id/rating', checkInput.addRating, booksCtrl.addRating);
+router.put('/:id', auth, upload.single('image'), checkInput.updateBook, booksCtrl.updateBook);
+router.delete('/:id', auth, checkInput.deleteBook, booksCtrl.deleteBook);
 
 module.exports = router;
